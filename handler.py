907ee1451d7b8d975b0f47bed53e9e7cc76c4528
Lanode <lanode@mail.ru>
2018-06-08 01:47:58 +0300

    Какая-то говнокодная хуйня

--- handler.py
+++ handler.py
@@ -2,20 +2,29 @@ import re
 import kbotlib
 import json
 import random
-
+# parsers
 import parsers.rule34
+import parsers.booru
+
+
+TAGS_BLACKLIST = json.loads(open('./config.json').read())['tags_blacklist']
 
 
 def handle_msg(msg):
-    msg['pretty_text'] = msg['text'].replace('+', ' ')
+    kbotlib.log_print('MsgID#' + str(msg['message_id']) + ' Начало обработки запроса')
+    msg['pretty_text'] = msg['text'].replace('+', ' ').lower()
     words = list(filter(None, re.split(r'\s*(\S+)\s*', msg['pretty_text'])))
     msg['tags'] = words[1:]
     msg['command'] = words[0]
 
-    if '!all' in msg['tags'].lower():
-        msg['tags'].extend({'-fur','-pony*','-friendship*','-scat*','-furry','-dragon','-guro','-animal_penis','-animal','-wolf','-fox','-webm','-my_little_pony','-monster*','-3d','-animal*','-ant','-insects','-mammal','-horse','-blotch','-deer','-real*','-shit','-everlasting_summer','-copro*','-wtf'})
+    print(msg['tags'])
 
     if '/hentai' in msg['command']:
+        if '!rtrd' not in msg['tags']:
+            msg['tags'].extend(TAGS_BLACKLIST)
+        else:
+            msg['tags'].remove('!rtrd')
+
         result = parsers.rule34.rule34(msg['tags'])
 
         random.shuffle(result)
@@ -25,6 +34,48 @@ def handle_msg(msg):
         for attach in result[start:start + 5]:
             if 'url' in attach:
                 print(attach['url'])
+
+            if attach['type'] == 'photo':
+                attachments.append({
+                    'type': 'photo',
+                    'media': attach['url'],
+                    'caption': ' '.join(attach['tags'])[0:199]
+                })
+
+            elif attach['type'] == 'video':
+                attachments.append({
+                    'type': 'video',
+                    'media': attach['url'],
+                    'caption': ' '.join(attach['tags'])[0:199],
+                })
+
+        if attachments != []:
+            tg_resp = kbotlib.tg_api('sendMediaGroup', {
+                'chat_id': msg['chat']['id'],
+                'media': json.dumps(attachments)
+            })
+            if tg_resp['ok'] is True:
+                kbotlib.log_print('MsgID#' + str(msg['message_id']) + ': Запрос успешно обработан')
+            else:
+                kbotlib.log_print('MsgID#' + str(msg['message_id']) + ': Error:' + tg_resp['description'])
+        else:
+            kbotlib.log_print('MsgID#' + str(msg['message_id']) + ': Ничего не найдено')
+            kbotlib.tg_api('sendMessage', {
+                'chat_id': msg['chat']['id'],
+                'text': 'По вашему запросу ничего не найдено!',
+                'reply_to_message_id': msg['message_id']
+            })
+
+    elif '/animeart' in msg['command']:
+        result = parsers.booru.booru(msg['tags'])
+
+        random.shuffle(result)
+
+        start = random.randint(0, len(result))
+        attachments = []
+        for attach in result[start:start + 5]:
+            # if 'url' in attach:
+            #     print(attach['url'])
 
             if attach['type'] == 'photo':
                 attachments.append({
--- parsers/rule34.py
+++ parsers/rule34.py
@@ -1,6 +1,5 @@
 import untangle, random, requests
 def rule34(req):
-    print(req)
     tor = {
         'http': 'socks5://127.0.0.1:9050',
         'https': 'socks5://127.0.0.1:9050'
