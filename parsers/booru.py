224c4c1a3d1064653824697629bc709533345aec
Lanode <lanode@mail.ru>
2018-06-08 01:56:34 +0300

    Какая-то говнокодная хуйня

---
+++ parsers/booru.py
@@ -0,0 +1,32 @@
+import untangle, random, requests
+def booru(req):
+    r = requests.post('http://safebooru.org/index.php',
+        params={
+            'page': 'dapi',
+            's': 'post',
+            'q': 'index',
+            'limit': '100',
+            'tags': ' '.join(req)
+        })
+    parse = untangle.parse(r.text)
+    if parse.posts['count'] != '0':
+        #randnum = random.randint(0,len(parse.posts.post))
+        ret = []
+        for booru_c in range(len(parse.posts.post)):
+            file_url = 'http:' + parse.posts.post[booru_c]['file_url']
+            tags = parse.posts.post[booru_c]['tags'].split(' ')
+            # pic = requests.get(file_url).content
+            file_type = file_url.split('.')[-1]
+
+            if file_type in ('jpeg', 'png', 'jpg'):
+                ret.append({"type": "photo", "from": "safebooru.org", "url": file_url, "tags": tags})
+
+            elif file_type == 'gif':
+                ret.append({"type": "gif", "from": "safebooru.org", "url": file_url, "tags": tags})
+
+            elif file_type == 'video':
+                ret.append({"type": "video", "from": "safebooru.org", "url": file_url, "tags": tags})
+
+        return ret
+    else:
+        return [{"type": "error", "from": "safebooru.org", "error_type": 1, "text": "Ничего не найдено"}]
